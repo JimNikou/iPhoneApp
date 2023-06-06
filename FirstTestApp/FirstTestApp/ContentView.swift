ca25e0ec4d4fb8be36c533318868eb7af8b7f000
Dimitrios Nikou <119452959+JimNikou@users.noreply.github.com>
2023-06-06 16:24:47 +0300

    init commit, first interaction with Swift

---
+++ FirstTestApp/FirstTestApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FirstTestApp
+//
+//  Created by Dimitrios Nikou on 19/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!sdfgsudyfgsu")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstTestApp/FirstTestApp/FirstTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTestAppApp.swift
+//  FirstTestApp
+//
+//  Created by Dimitrios Nikou on 19/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
